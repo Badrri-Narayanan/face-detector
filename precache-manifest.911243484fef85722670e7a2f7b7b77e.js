955d2169fd06a936c3cf0efdd59e7ed80a482c36
badrri-narayanan <badrri1995@gmail.com>
2020-08-23 18:08:14 +0530

    This is the front end of the Face Detector Application.

---
+++ precache-manifest.911243484fef85722670e7a2f7b7b77e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9c7078e5e8367b51715fb71fd1e93c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29fc7ac6bc34b604ee6f",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bdb7220f0981b185b9ec",
+    "url": "/static/css/main.ddc1daa4.chunk.css"
+  },
+  {
+    "revision": "29fc7ac6bc34b604ee6f",
+    "url": "/static/js/2.3fc3d911.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3fc3d911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb7220f0981b185b9ec",
+    "url": "/static/js/main.4d9b82e7.chunk.js"
+  },
+  {
+    "revision": "efd5eb32c556b578ba10",
+    "url": "/static/js/runtime-main.bdede494.js"
+  }
+]);
