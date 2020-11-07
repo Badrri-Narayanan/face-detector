3bb247016e159b8111e4020891e9e28080c468a5
[badrri-narayanan] <[badrri1995@gmail.com]>
2020-11-07 21:45:53 +0530

    Updates

---
+++ precache-manifest.3066d3f584e5f516295f7bc1af29cfca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e9f7d033b12b24037149e62a1a4045",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "9e7e6679ef12d196e818",
+    "url": "/face-detector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3ec0bc00f0f0a769cf32",
+    "url": "/face-detector/static/css/main.395a205e.chunk.css"
+  },
+  {
+    "revision": "9e7e6679ef12d196e818",
+    "url": "/face-detector/static/js/2.c1d4bb25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detector/static/js/2.c1d4bb25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec0bc00f0f0a769cf32",
+    "url": "/face-detector/static/js/main.5c16f2cd.chunk.js"
+  },
+  {
+    "revision": "a549d34096ec0a7cbab6",
+    "url": "/face-detector/static/js/runtime-main.1d9f71e6.js"
+  }
+]);
